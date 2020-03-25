26548d7d27ddf9dee965c3ab385b6ee6a1bc2bb3
Johan Mabille <johan.mabille@gmail.com>
2020-03-25 12:33:22 +0100

    Release 0.6.13

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
-cmake -DCMAKE_BUILD_TYPE=Release     \
-      -DCMAKE_INSTALL_PREFIX=$PREFIX \
-      -DDISABLE_ARCH_NATIVE=ON       \
-      -DCMAKE_INSTALL_LIBDIR=lib     \
-      -DPYTHON_EXECUTABLE=$PYTHON    \
-      $SRC_DIR
+if [[ ${target_platform} == linux-ppc64le ]]; then
+  cmake -DCMAKE_BUILD_TYPE=Release     \
+        -DCMAKE_INSTALL_PREFIX=$PREFIX \
+        -DXPYT_DISABLE_TUNE_GENERIC=ON \
+        -DCMAKE_INSTALL_LIBDIR=lib     \
+        -DPYTHON_EXECUTABLE=$PYTHON    \
+        $SRC_DIR
+else
+  cmake -DCMAKE_BUILD_TYPE=Release     \
+        -DCMAKE_INSTALL_PREFIX=$PREFIX \
+        -DXPYT_DISABLE_ARCH_NATIVE=ON  \
+        -DCMAKE_INSTALL_LIBDIR=lib     \
+        -DPYTHON_EXECUTABLE=$PYTHON    \
+        $SRC_DIR
+fi
 
 make install
